fe706b2ab79ee63e83e88b2d730869550729f899
Sidney Andrews <sidney@seesharprun.net>
2018-02-01 16:07:37 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SimpleCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello Everyone!");
+            Console.WriteLine("It's great to be here!");
+            Console.ReadKey();
+        }
+    }
+}
